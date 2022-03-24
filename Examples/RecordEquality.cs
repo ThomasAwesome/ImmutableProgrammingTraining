10d64401683b9d7eb546b5fa80c6e4996b8b35de
Thomas Camargo <camargothomas@gmail.com>
2022-03-24 08:14:53 -0600

    Add example for simple equality.

---
+++ Examples/RecordEquality.cs
@@ -0,0 +1,27 @@
+﻿namespace ImmutableProgrammingTraining.Examples;
+
+public record RecordEquality(string Name, string Type)
+{
+  //You don't have to override Equals. It needs to either virtual or the record needs to be sealed.
+  //this is pretty close to what the default would be.
+  public virtual bool Equals(RecordEquality? other)
+  {
+    if (ReferenceEquals(other, null))
+    {
+      return false;
+    }
+
+    if (ReferenceEquals(this, other))
+    {
+      return true;
+    }
+
+    return Name.Equals(other.Name) && Type.Equals(other.Type);
+  }
+
+  public override int GetHashCode()
+  {
+    return HashCode.Combine(Name, Type);
+  }
+}
+
